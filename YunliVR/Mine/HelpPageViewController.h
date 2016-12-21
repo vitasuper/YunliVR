172923fd78ac4647e9f3260b685b309e6407f8ca
vitasuper <vitasuperh@gmail.com>
2016-12-21 10:25:27 +0800

    Add 2 pages (ABOUT page and HELP page)

---
+++ YunliVR/Mine/HelpPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelpPageViewController.h
+//  YunliVR
+//
+//  Created by 韩龙粤 on 2016/12/20.
+//  Copyright © 2016年 vitasuper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpPageViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *helpTextView;
+
+@end
---
+++ YunliVR/Mine/HelpPageViewController.m
@@ -0,0 +1,39 @@
+//
+//  HelpPageViewController.m
+//  YunliVR
+//
+//  Created by 韩龙粤 on 2016/12/20.
+//  Copyright © 2016年 vitasuper. All rights reserved.
+//
+
+#import "HelpPageViewController.h"
+
+@interface HelpPageViewController ()
+
+@end
+
+@implementation HelpPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.helpTextView.editable = NO;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
